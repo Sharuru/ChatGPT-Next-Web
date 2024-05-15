63f8cbbb3de742e0a5d8b40e2b279113a5bf04cd
Sharuru <mave@foxmail.com>
2024-05-15 22:03:50 +0800

    add gpt-4o

--- app/constant.ts
+++ app/constant.ts
@@ -105,6 +105,14 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
+    name: "gpt-4o",
+    available: true,
+  },
+  {
+    name: "gpt-4o-2024-05-13",
+    available: true,
+  },
+  {
     name: "gpt-4-turbo-2024-04-09",
     available: true,
   },
